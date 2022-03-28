94205490bff7705cad32635cb128724bdfd7f7b7
Stephan_Melnicov <stef200605@gmail.com>
2022-03-28 21:10:40 +0500

    Результат отбора

--- 2022.02.16WEB6/HW/1.py
+++ 2022.02.16WEB6/HW/1.py
@@ -1,7 +1,8 @@
 from flask import Flask, url_for, request, render_template
 
 app = Flask(__name__)
-data = {'Марс': ['Эта планета близка к земли', 'Есть вода и ресурсы', 'Небольшое МП', 'Она просто красивая, красная', "Хочу туда"],
+data = {'Марс': ['Эта планета близка к земли', 'Есть вода и ресурсы', 'Небольшое МП', 'Она просто красивая, красная',
+                 "Хочу туда"],
         'Земля': ['Мы тут живём', 'И ещё много все', 'Очень много', 'Воды', 'И тчо-то ещё'],
         'Венера': ['Что-то', 'Не, я действительно мало знаю', 'Очень мало', 'Я тупой(((((', 'Ну и ладно'],
         'Солнышко': ['Тёплое', 'Очень тёпленькое', 'Крайне тёпленькое', 'Солнышко)))))', 'Большое'],
@@ -23,6 +24,11 @@ def choice(planet_name):
     global data
     lst = data.get(planet_name, 'Земля')
     return render_template('planet.html', data=lst, planet_name=planet_name)
+
+
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level: int, rating: float):
+    return render_template('result.html', nickname=nickname, level=level, rating=rating)
 
 
 if __name__ == '__main__':
